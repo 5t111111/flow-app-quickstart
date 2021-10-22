d5a13cb741bdc1b3f9bb1c43c6dcd705b4f89dc6
Hirofumi Wakasugi <baenej@gmail.com>
2021-10-22 16:01:41 +0900

    Tutorial Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,114 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as fcl from "@onflow/fcl";
 import "./config";
 
+interface User {
+  loggedIn: boolean | null;
+  addr?: string;
+}
+
 function App() {
+  const [user, setUser] = useState<User>({ loggedIn: null });
+  const [name, setName] = useState("");
+  const [transactionStatus, setTransactionStatus] = useState<any>(null);
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg: any, t: any) => [arg(user.addr, t.Address)],
+    });
+
+    setName(profile?.name ?? "No Profile");
+  };
+
+  const initAccount = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction {
+          prepare(account: AuthAccount) {
+            // Only initialize the account if it hasn't already been initialized
+            if (!Profile.check(account.address)) {
+              // This creates and stores the profile in the user's account
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+              // This creates the public capability that lets applications read the profile's info
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+            }
+          }
+        }
+      `,
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50,
+    });
+
+    const transaction = await fcl.tx(transactionId).onceSealed();
+    console.log(transaction);
+  };
+
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg: any, t: any) => [arg("Flow Developer!", t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50,
+    });
+
+    fcl
+      .tx(transactionId)
+      .subscribe((res: any) => setTransactionStatus(res.status));
+  };
+
+  const AuthedState = () => {
+    return (
+      <div>
+        <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <div>Transaction Status: {transactionStatus ?? "--"}</div>
+        <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button>
+        <button onClick={executeTransaction}>Execute Transaction</button>
+        <button onClick={fcl.unauthenticate}>Log Out</button>
+      </div>
+    );
+  };
+
+  const UnauthenticatedState = () => {
+    return (
+      <div>
+        <button onClick={fcl.logIn}>Log In</button>
+        <button onClick={fcl.signUp}>Sign Up</button>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h1>Flow App</h1>
+      {user.loggedIn ? <AuthedState /> : <UnauthenticatedState />}
     </div>
   );
 }
--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ import { config } from "@onflow/fcl";
 config({
   "accessNode.api": process.env.REACT_APP_ACCESS_NODE_API,
   "discovery.wallet": process.env.REACT_APP_DISCOVERY_WALLET,
+  "0xProfile": process.env.REACT_APP_PROFILE_ADDRESS,
 });
